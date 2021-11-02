437e2c003262ac84a9f059eb158980b7ad947848
olegharp <px845pev@mail.ru>
2021-11-02 11:24:20 +0300

    removed gitignored files

---
+++ Migrations/20211102080521_Index.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmsCatalog.Migrations
+{
+    public partial class Index : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_ReleaseYear",
+                table: "Movie",
+                column: "ReleaseYear");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Movie_ReleaseYear",
+                table: "Movie");
+        }
+    }
+}
